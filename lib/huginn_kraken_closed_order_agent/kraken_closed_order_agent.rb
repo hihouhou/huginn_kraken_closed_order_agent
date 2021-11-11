a0f5f0646ff243ddbba593dd7f06de8477cb0692
Nicolas Germain <ngermain@hihouhou.com>
2021-11-11 10:35:22 +0100

    bad condition updated

--- lib/huginn_kraken_closed_order_agent/kraken_closed_order_agent.rb
+++ lib/huginn_kraken_closed_order_agent/kraken_closed_order_agent.rb
@@ -155,21 +155,17 @@ module Agents
             last_status = memory['last_status'].gsub("=>", ": ").gsub(": nil,", ": null,")
             last_status = JSON.parse(last_status)
             payload['result']['closed'].each do | k, v |
-            if interpolated['debug'] == 'true'
-              log "last_status"
-              log last_status
-            end
               found = false
               last_status.each do | kbis, vbis|
-                if k == kbis && v == vbis
+                if k == kbis
                   found = true
-                  if interpolated['debug'] == 'true'
-                    log "found is #{found}"
-                  end
                 end
               end
+              if interpolated['debug'] == 'true'
+                log "found is #{found}"
+              end
               if found == false
-                  create_event payload: v
+                create_event payload: v
               end
             end
           end
